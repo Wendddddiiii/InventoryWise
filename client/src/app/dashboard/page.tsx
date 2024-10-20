6fefee6d45c21ba4d195fafcbb4f6e15cab8961d
Wendddddiiii <wenditang058@gmail.com>
2024-10-20 16:28:26 +1100

    complete dashboard homepage

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import CardPurchaseSummary from "./CardPurchaseSummary";
 import CardSalesSummary from "./CardSalesSummary";
 import CardExpenseSummary from './CardExpenseSummary';
 import StatCard from "./StatCard";
-import { Package, TrendingDown, TrendingUp } from "lucide-react";
+import { CheckCircle, Package, Tag, TrendingDown, TrendingUp } from "lucide-react";
 
 const Dashboard = () => {
   return (
@@ -21,6 +21,24 @@ const Dashboard = () => {
         details={[
           {title: "Customer Growth", amount:"368.00", changePercentage:131, IconComponent: TrendingUp,},
           {title: "Expenses", amount:"100.00", changePercentage:-20, IconComponent: TrendingDown,},
+        ]}
+      />
+      <StatCard 
+        title = "Dues & Pending Orders"
+        primaryIcon={<CheckCircle className="text-blue-600 w-6 h-6" />}
+        dateRange="11 - 21 Oct 2024"
+        details={[
+          {title: "Dues", amount:"350.00", changePercentage:56, IconComponent: TrendingUp,},
+          {title: "Pending Orders", amount:"98", changePercentage:-12, IconComponent: TrendingDown,},
+        ]}
+      />
+      <StatCard 
+        title = "Sales & Discount"
+        primaryIcon={<Tag className="text-blue-600 w-6 h-6" />}
+        dateRange="11 - 21 Oct 2024"
+        details={[
+          {title: "Sales", amount:"1680.00", changePercentage:40, IconComponent: TrendingUp,},
+          {title: "Discount", amount:"160.00", changePercentage:-8, IconComponent: TrendingDown,},
         ]}
       />
     </div>
